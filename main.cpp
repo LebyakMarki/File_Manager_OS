ef4a1f4a9722310bce0d4581ea2b76d7a373c05d
LebyakMarki <lebyak.m@gmail.com>
2020-11-23 00:24:17 +0200

    Started working on search.
    Added logo also

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -7,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("KManager");
-    w.setWindowIcon(QIcon(":./icons/logo.png"));
+    w.setWindowIcon(QIcon(":/images/icons/logo.png"));
     w.show();
     return a.exec();
 }
